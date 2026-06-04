1c1a513266c039f9268239690a61cac2e64298e2
cordsofc-png <cordsofc@gmail.com>
2026-06-04 12:13:59 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "helping cords rn",
+        name: "im only for cords frfr",
         // Activity type number (0 = Playing).
         type: 1, 
       },
